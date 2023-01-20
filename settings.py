cf639f6da79b36318db9b6aa1acc1e1742fe86d1
Zhargal <115886034+lagrahz@users.noreply.github.com>
2023-01-20 20:56:04 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = 'C:\Users\Zhargal\PycharmProjects\28/chromedriver.exe'
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'jargalkhas@mail.ru'
+    valid_password = '12345Qwer!'
+    valid_email_for_reg = 'jargalkhas@gmail.com'
+    invalid_email = 'testurro@mail.gref'
+    invalid_password = '123POI90!'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Тест'
+    l_name = 'Скиллфактори'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'и'
+    latin_generate_string = 'ssttww'
+    chinese_chars = '我不会说汉语请做不辣的怎么样你会说英语'
+    special_chars = '!@#$$%^&*()_+_+=//{}'
+    numbers = 987
+    empty = ''
+    passw1 = 'qQ123987'
+    passw2 = 'Qw12345678Wq543'
+    passw3 = 'Qa12345678Er12345978'
